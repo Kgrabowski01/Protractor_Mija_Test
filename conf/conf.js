6e689d38dcc4897c44312283ac1ea9512f458fb0
Kgrabowski01 <grabowski.konrad01@gmail.com>
2015-11-18 22:51:15 +0100

    Update conf.js

--- conf/conf.js
+++ conf/conf.js
@@ -1,13 +1,1 @@
-exports.config = {
-  framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  capabilities: { 'browserName': 'firefox' },
-  specs: ['spec.js'],
-  onPrepare: function() {
-    browser.manage().window().setSize(1600, 900);
-  },
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000
-  }
-};
+
